edd1e393db87fdc55fb3d020dd0f917a4bbed6f1
carpenta <viruscih@gmail.com>
2013-06-22 00:09:50 +0900

    model data insert and select test

--- main.py
+++ main.py
@@ -16,56 +16,54 @@ extensions=['jinja2.ext.autoescape'])
 
 # Models
 class Participant(ndb.Model):
-  image = ndb.StringProperty(indexed=False)
-  name = ndb.StringProperty(indexed=False)
-	association = ndb.StringProperty(indexed=False)
-	emblem = ndb.StringProperty(indexed=False)
+	image = ndb.StringProperty()
+	name = ndb.StringProperty()
+	association = ndb.StringProperty()
+	emblem = ndb.StringProperty()
+	operator = ndb.UserProperty()
 	date = ndb.DateTimeProperty(auto_now_add=True)
 
 class FightMatch(ndb.Model):
 	section_num = ndb.StringProperty()
 	match_num = ndb.StringProperty()
-  participant1 = ndb.ReferenceProperty(Participant)  
-	participant2 = ndb.ReferenceProperty(Participant)
+	participant1 = ndb.KeyProperty(kind=Participant)  
+	participant2 = ndb.KeyProperty(kind=Participant)
 	score = ndb.StringProperty()
-	checklist = ndb.StringListProperty()
-	winner = ndb.ReferenceProperty(Participant)
+	checklist = ndb.StringProperty(repeated=True)
+	winner = ndb.KeyProperty(kind=Participant)
 	result = ndb.StringProperty()
+	operator = ndb.UserProperty()
+	date = ndb.DateTimeProperty(auto_now_add=True)
 
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        if users.get_current_user():
-            url = users.create_logout_url(self.request.uri)
-            template_values = {}
-            template = JINJA_ENVIRONMENT.get_template('index.html')
-            self.response.write(template.render(template_values))
-        else:
-            self.redirect(users.create_login_url(self.request.uri))
+	def get(self):
+		if users.get_current_user():
+			url = users.create_logout_url(self.request.uri)
+			template_values = {'participants' : Participant.query()}
+			template = JINJA_ENVIRONMENT.get_template('index.html') 
+			self.response.write(template.render(template_values))
+		else:
+			self.redirect(users.create_login_url(self.request.uri))
 
 class Schedule(webapp2.RequestHandler):
-		def get(self):
-			self.response.write("test");
-
-    def post(self):
-        if users.get_current_user():
-            checkedScheduleJson = self.request.get('schedule', '')
-            checkedPlaceJson = self.request.get('place', '')
-
-            user = users.get_current_user()
-            userId = user.user_id()
-            userSchedule = UserCheckedSchedule(ndb.Key('UserCheckedSchedule', userId))
-
-            userSchedule.checkedDateJson = checkedScheduleJson
-            userSchedule.checkedPlaceJson = checkedPlaceJson
-            userSchedule.put()
-            self.response.write("success")
-        else:
-            self.response.write("fail")
-
-
-
+	def get(self):
+		self.response.write("test");
+	def post(self): 
+		if users.get_current_user():	
+			participant = Participant()
+			participant.operator = users.get_current_user()
+			participant.name = self.request.get('user_name', 'unknown')
+			participant.image = "not yet"
+			participant.association = "test"
+			participant.emblem = "not yet"
+			participant.put()
+			self.response.write("<a href='/'>success</a>")
+		else:
+			self.response.write("fail")
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/update', Schedule)
 ], debug=True)
+
+
